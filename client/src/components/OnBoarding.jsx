c2cbf09c6b05fe5f51f560c9b3f3a3e87018374f
Fredyrooh <fredyrooh@gmail.com>
2022-11-25 17:36:57 -0300

    configuracion de tailwind

--- client/src/components/OnBoarding.jsx
+++ client/src/components/OnBoarding.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 export const OnBoarding = () => {
   return (
-    <div>OnBoardingasdasdasdasdasd</div>
+    <div className='bg-red-100 '>
+
+    <h1 className="text-3xl text-red-500 underline">
+    Hello world!
+  </h1>
+ <div className='text-center'>
+ <button className='bg-black text-white rounded-xl bg-contain p-4'>este es un boton</button>
+ </div>
+  </div>
   )
 }
